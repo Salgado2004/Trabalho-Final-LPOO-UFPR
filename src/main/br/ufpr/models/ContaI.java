bc31485e80519826c46f4ecb2126814d290d6a38
salgado2004 <leosalgado2004@gmail.com>
2024-05-15 21:47:02 -0300

    feat: create interface ContaI

--- src/main/br/ufpr/models/ContaI.java
+++ src/main/br/ufpr/models/ContaI.java
@@ -1,5 +1,10 @@
 package main.br.ufpr.models;
 
 public interface ContaI {
-
+    public boolean deposita(double valor) ;
+    public boolean saca(double valor) ;
+    public Cliente getDono() ;
+    public int getNumero() ;
+    public double getSaldo() ;
+    public void remunera() ;
 }
