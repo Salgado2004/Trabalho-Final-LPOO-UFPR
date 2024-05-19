4fd284c54a83ca4fc7447edd85c9a58cfbdf5e3b
raulbana <raulfbana@gmail.com>
2024-05-19 13:07:04 -0300

    feat: abstract class Conta

--- src/main/br/ufpr/models/Conta.java
+++ src/main/br/ufpr/models/Conta.java
@@ -1,4 +1,46 @@
 package main.br.ufpr.models;
 
-public class Conta {
+public abstract class Conta implements ContaI {
+    protected int numero;
+    protected Cliente dono;
+    protected double saldo;
+    protected double limite;
+
+    public Conta(int numero, Cliente dono, double saldo, double limite) {
+        this.numero = numero;
+        this.dono = dono;
+        this.saldo = saldo;
+        this.limite = limite;
+    }
+
+
+    public boolean depositar(double valor) {
+        if (valor > 0) {
+            this.saldo += valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sacar(double valor) {
+        if (valor > 0 && valor <= this.saldo + this.limite) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public Cliente getDono() {
+        return this.dono;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public abstract void remunera();
 }
