2bf266504ff12e9002472c6a8e55dc4e52c02ba9
salgado2004 <leosalgado2004@gmail.com>
2024-05-30 00:01:04 -0300

    CrFeat: Csse padrão de mensagens de diálogo

---
+++ src/main/br/ufpr/controllers/Mensagens.java
@@ -0,0 +1,27 @@
+package main.br.ufpr.controllers;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Mensagens {
+    private static final ImageIcon erroIcon = new ImageIcon("assets/error.png");
+    private static final ImageIcon warningIcon = new ImageIcon("assets/warning.png");
+    private static final ImageIcon successIcon = new ImageIcon("assets/sucess.png");
+    private static final ImageIcon loadingIcon = new ImageIcon("assets/load.png");
+
+    public static void erro(Component origin, String mensagem){
+        JOptionPane.showMessageDialog(origin, mensagem, "Erro", JOptionPane.ERROR_MESSAGE, erroIcon);
+    }
+
+    public static void aviso(Component origin, String mensagem){
+        JOptionPane.showMessageDialog(origin, mensagem, "Aviso", JOptionPane.WARNING_MESSAGE, warningIcon);
+    }
+
+    public static void sucesso(Component origin, String mensagem){
+        JOptionPane.showMessageDialog(origin, mensagem, "Sucesso", JOptionPane.INFORMATION_MESSAGE, successIcon);
+    }
+
+    public static void carregando(Component origin, String mensagem){
+        JOptionPane.showMessageDialog(origin, mensagem, "Carregando", JOptionPane.INFORMATION_MESSAGE, loadingIcon);
+    }
+}
