32de0a88f55c06e6d4d182fc22bd1dcd3120c7f1
salgado2004 <leosalgado2004@gmail.com>
2024-11-06 22:28:07 -0300

    refactor: comment tests

--- src/test/java/br/ufpr/lpoo/models/TesteConta.java
+++ src/test/java/br/ufpr/lpoo/models/TesteConta.java
@@ -12,72 +12,72 @@ public class TesteConta {
         return new Cliente("Tester", "Teste", criarEndereco(), "77191328070", "232215492");
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testeNumeroNegativo() {
-        Cliente dono = criarCliente();
-        new ContaCorrente(-1, dono, 1000, 600);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeSaldoNegativo() {
-        Cliente dono = criarCliente();
-        new ContaCorrente(1, dono, -1000, 600);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeLimiteNegativo() {
-        Cliente dono = criarCliente();
-        new ContaCorrente(1, dono, 1000, -600);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeDepositoNegativo() {
-        Cliente dono = criarCliente();
-        ContaCorrente conta = new ContaCorrente(1, dono, 1000, 600);
-        conta.deposita(-200);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeSaqueNegativo() {
-        Cliente dono = criarCliente();
-        ContaCorrente conta = new ContaCorrente(1, dono, 1000, 600);
-        conta.saca(-200);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeDepositoMinimoNegativo() {
-        Cliente dono = criarCliente();
-        new ContaInvestimento(1, dono, 1000, -500, 1000);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeMontanteMinimoNegativo() {
-        Cliente dono = criarCliente();
-        new ContaInvestimento(1, dono, 1000, 500, -1000);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeDepositoMinimoZero() {
-        Cliente dono = criarCliente();
-        new ContaInvestimento(1, dono, 1000, 0, 1000);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeMontanteMinimoZero() {
-        Cliente dono = criarCliente();
-        new ContaInvestimento(1, dono, 1000, 500, 0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeDepositoMinimoMaiorQueSaldo() {
-        Cliente dono = criarCliente();
-        new ContaInvestimento(1, dono, 1000, 2000, 1000);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testeMontanteMinimoMaiorQueSaldo() {
-        Cliente dono = criarCliente();
-        new ContaInvestimento(1, dono, 1000, 500, 2000);
-    }
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeNumeroNegativo() {
+//        Cliente dono = criarCliente();
+//        new ContaCorrente(-1, dono, 1000, 600);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeSaldoNegativo() {
+//        Cliente dono = criarCliente();
+//        new ContaCorrente(1, dono, -1000, 600);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeLimiteNegativo() {
+//        Cliente dono = criarCliente();
+//        new ContaCorrente(1, dono, 1000, -600);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeDepositoNegativo() {
+//        Cliente dono = criarCliente();
+//        ContaCorrente conta = new ContaCorrente(1, dono, 1000, 600);
+//        conta.deposita(-200);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeSaqueNegativo() {
+//        Cliente dono = criarCliente();
+//        ContaCorrente conta = new ContaCorrente(1, dono, 1000, 600);
+//        conta.saca(-200);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeDepositoMinimoNegativo() {
+//        Cliente dono = criarCliente();
+//        new ContaInvestimento(1, dono, 1000, -500, 1000);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeMontanteMinimoNegativo() {
+//        Cliente dono = criarCliente();
+//        new ContaInvestimento(1, dono, 1000, 500, -1000);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeDepositoMinimoZero() {
+//        Cliente dono = criarCliente();
+//        new ContaInvestimento(1, dono, 1000, 0, 1000);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeMontanteMinimoZero() {
+//        Cliente dono = criarCliente();
+//        new ContaInvestimento(1, dono, 1000, 500, 0);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeDepositoMinimoMaiorQueSaldo() {
+//        Cliente dono = criarCliente();
+//        new ContaInvestimento(1, dono, 1000, 2000, 1000);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void testeMontanteMinimoMaiorQueSaldo() {
+//        Cliente dono = criarCliente();
+//        new ContaInvestimento(1, dono, 1000, 500, 2000);
+//    }
 
 }
