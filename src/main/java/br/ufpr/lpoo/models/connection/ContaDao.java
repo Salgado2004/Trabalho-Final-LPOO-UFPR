3d015abd4ba08e5ea29f6e461c56ce72323bca73
salgado2004 <leosalgado2004@gmail.com>
2024-11-17 17:44:39 -0300

    feat: config interface DAOs
    Co-authored-by: AlissonGSantos <alisson.gabriel@ufpr.br>
    Co-authored-by: Pedro-H108 <pedro.souza.1008@gmail.com>
    Co-authored-by: raulbana <raul.ferreira@ufpr.br>

--- src/main/java/br/ufpr/lpoo/models/connection/ContaDao.java
+++ src/main/java/br/ufpr/lpoo/models/connection/ContaDao.java
@@ -2,6 +2,7 @@ package br.ufpr.lpoo.models.connection;
 
 import br.ufpr.lpoo.models.Conta;
 
-public interface ContaDao extends Dao<Conta>{
+public interface ContaDao extends Dao<Conta> {
     Conta getByNumero(String numero);
+    Conta getByCliente(String cpfCliente);
 }
